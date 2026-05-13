7089e300676b17f47597bf58d54686b244433dd7
varaprasad-rasuri-iraspace <varaprasad.r1002@gmail.com>
2026-05-13 07:34:28 +0530

    commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+REM node C:\code\coaching\vara\git-change-validator\dist\cli.js validate --preview-payload --summary-only --comments --jira KAN-1 --from origin/dev --branch origin/kan-1-leetcode-link-fix --ai-stage-a-preview
+node C:\code\coaching\vara\git-change-validator\dist\cli.js validate
