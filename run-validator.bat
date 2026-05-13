57a2d16a1f479611880c86ea8f19c87267896bef
varaprasad-rasuri-iraspace <varaprasad.r1002@gmail.com>
2026-05-13 09:43:25 +0530

    commit

--- run-validator.bat
+++ run-validator.bat
@@ -1,6 +1,28 @@
+call cls
+
 echo off
 REM node C:\code\coaching\vara\git-change-validator\dist\cli.js validate --preview-payload --summary --comments --jira KAN-1 --from origin/dev --branch origin/kan-1-leetcode-link-fix --ai-stage-a-preview
+
+echo NOTE:
+echo =======
+echo Before running this:
+echo '
+echo 1 Be in YOUR PROJECT GIT repository
+echo  cd my-project-repo
+echo '
+echo 2 Copy file (s) from validator repo
+echo  my-project-repo/.env
+echo  my-project-repo/validator.config.json
+echo  my-project-repo/run-validator.bat
+echo '
+echo 3 Run validator
+echo  cd my-project-repo
+echo  run-validator.bat
+echo '
+echo =======
+
 echo on
 
-call copy /Y C:\code\coaching\vara\git-change-validator\validator.config.json .
-call node C:\code\coaching\vara\git-change-validator\dist\cli.js validate
+REM call copy /Y C:\code\coaching\vara\git-change-validator\validator.config.json .
+
+PAUSE Press enter, if above are done. Else Ctrl+C to cancel && call node C:\code\coaching\vara\git-change-validator\dist\cli.js validate
