3a1a52139d0e9709b8451f0795c26aad9aa114d3
varaprasad-rasuri-iraspace <varaprasad.r1002@gmail.com>
2026-05-18 07:33:25 +0530

    commit

--- 4.run-validator-extension.bat
+++ 4.run-validator-extension.bat
@@ -1,7 +1,6 @@
 @echo off
 setlocal
 
-set "VALIDATOR_REPO=C:\code\coaching\vara\git-change-validator"
-
 echo Launching VS Code with the extension loaded...
+
 code --new-window --extensionDevelopmentPath="C:\code\coaching\vara\git-change-validator" "."
