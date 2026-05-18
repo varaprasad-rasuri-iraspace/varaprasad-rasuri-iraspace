c2418cacd685019daa10c1a1ed2578701bfa63ad
varaprasad-rasuri-iraspace <varaprasad.r1002@gmail.com>
2026-05-18 07:40:55 +0530

    commit

--- 4.run-validator-extension.bat
+++ 4.run-validator-extension.bat
@@ -1,6 +1,14 @@
 @echo off
 setlocal
 
+echo off
+
 echo Launching VS Code with the extension loaded...
 
-code --new-window --extensionDevelopmentPath="C:\code\coaching\vara\git-change-validator" "."
+echo NOTE:
+echo 1. copy the latest file 4.run-validator-extension.bat from the validator repo
+echo 2. Once launched, ctrl+shift+p, Git Change Validation: Open Panel
+
+echo Press ENTER key if above are done. Ctrl+C to cancel
+
+PAUSE && call code --new-window --extensionDevelopmentPath="C:\code\coaching\vara\git-change-validator" "."
